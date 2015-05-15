ae56f7c71c0187bbb00999ec9aee2e726c6e65c6
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-05-14 19:22:26 -0700

    Checkpoint 21 complete, Static Pages

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+#Generated WelcomeController class by `rails g controller welcome index about` command
+class WelcomeController < ApplicationController
+
+  #index references the auto-generated view associated with WelcomeController
+  def index
+  end
+
+  #about references the auto-generated view associated with WelcomeController
+  def about
+  end
+end

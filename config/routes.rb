c34a45f0ea98bf08872efd0c7729262dfd5abc09
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-05-17 21:11:14 -0700

    Checkpoint 27 CRUD complete. Create PostController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
-  
+  resources :posts
+
+  get 'about' => 'welcome#about'
+
   #this is an implied hash - could be written as root({to: 'welcome#index'})
   root to: 'welcome#index' 
 

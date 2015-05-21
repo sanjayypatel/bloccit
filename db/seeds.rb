6df1099a669ea652e606bd512379935fdf914381
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-05-20 23:19:38 -0700

    Assignment 29 More CRUD complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,37 @@
 require 'faker'
 
-# Create 50 Posts
-50.times do 
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
-    )
-end
 
-posts = Post.all
+unless Post.any?
+  # Create 50 Posts
+  50.times do 
+    Post.create!(
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph
+      )
+  end
 
-# Create 100 Comments, associated with random Posts
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-    )
+  posts = Post.all
+
+  # Create 100 Comments, associated with random Posts
+  100.times do
+    Comment.create!(
+      post: posts.sample,
+      body: Faker::Lorem.paragraph
+      )
+  end
+end 
+
+unless Question.any?
+  25.times do
+    Question.create!(
+      title: Faker::Lorem.sentence + '?',
+      body: Faker::Lorem.paragraph + '?',
+      resolved: false
+      )
+  end
 end
 
 puts "Seeds finished."
 puts "#{Post.count} posts created."
 puts "#{Comment.count} comments created."
+puts "#{Question.count} comments created."

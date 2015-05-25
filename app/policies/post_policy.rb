100dcdf9113bdd97dcb7426ac24cbdb11d8ac841
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-05-25 13:14:05 -0700

    Assignment 33 Authorization Complete.

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,20 @@ class PostPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  class Scope < Scope
+
+    def resolve
+      if user.present?
+        if user.admin? || user.moderator?
+          scope.all
+        else
+          scope.where(user: user.id)
+        end
+      else
+        scope.none
+      end
+    end
+
+  end
 end

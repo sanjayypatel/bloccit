c1ef41da8c2e2c31ae0c55077251f1e4149bd926
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-05-24 23:14:58 -0700

    Assignment 32 Scopes Complete. Added 2 new scopes to Post Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+  
+  # Replacing a default_scope of reveresed created_at order with two more, specific scopes
+  # default_scope { order('created_at DESC')}
+  scope :ordered_by_title, -> { order(title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { order(created_at: :desc)}
 
-  default_scope { order('created_at DESC')}
 end

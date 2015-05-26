bdac602c0eefcac34af167a34484422fd9a21316
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-05-25 19:25:37 -0700

    Checkpoint 34 Topics, Complete.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,27 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-    authorize @posts
-  end
 
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    # look at routes - Rails convention dictates that the params hash will have a :topic_id
+    authorize @post
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize @post
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])
     #params #=> { :post => {title: "form value", body: "form value"}, commit: "Save"}
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post.topic = @topic
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving post. Please try again."
       render :new
@@ -27,16 +29,18 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,20 @@ require 'faker'
 end
 users = User.all
 
+# Create 15 Topics
+15.times do
+  topic = Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
 
 # Create 50 Posts
 50.times do 
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -32,15 +41,35 @@ posts = Post.all
     )
 end
 
-# Modify first user to allow dev login and testing
-user = User.first
-user.skip_reconfirmation!
-user.update_attributes!(
-  email: 'sanjay.y.patel@gmail.com',
-  password: 'helloworld'
+# Create 3 additional users - Admin, Moderator, and Member for testing
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
 )
+admin.skip_confirmation!
+admin.save!
+
+moderator = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  role: 'moderator'
+  )
+moderator.skip_confirmation!
+moderator.save!
+
+member = User.new(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld'
+  )
+member.skip_confirmation!
+member.save!
 
 puts "Seeds finished."
 puts "#{User.count} users created."
+puts "#{Topic.count} topics created."
 puts "#{Post.count} posts created."
 puts "#{Comment.count} comments created."

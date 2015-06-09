f33192f0f02d9f1e42bada2f7ee11ac3da4ae321
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-06-09 15:07:04 -0700

    Checkpoint 50, Mocking, Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,11 +10,9 @@ class Post < ActiveRecord::Base
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  # validates :user, presence: true
-  # validates :topic, presence: true
+  validates :user, presence: true
+  validates :topic, presence: true
   
-  after_create :create_vote
-
   def up_votes
     votes.where(value: 1).count
   end
@@ -33,8 +31,6 @@ class Post < ActiveRecord::Base
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
   end
-
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,22 +1,33 @@
 require 'rails_helper'
 
-describe 'Vote' do
-  describe 'validations' do
+describe Vote do
 
+  include TestFactories
+
+  describe 'validations' do
     describe 'value validations' do
       it "only allows values of 1 or -1" do
         # @post is needs to be defined to allow for after_save action in Vote model
-        @post = Post.create(title: 'post title', body: 'Post bodies must be pretty long.')
-        @vote_one = Vote.create(value: 1, post: @post)
-        @vote_two = Vote.create(value: -1, post: @post)
-        @vote_three = Vote.create(value: 2, post: @post)
+        post = associated_post
+        @vote_one = Vote.create(value: 1, post: post)
+        @vote_two = Vote.create(value: -1, post: post)
+        @vote_three = Vote.create(value: 2, post: post)
 
         expect(@vote_one.valid?).to eq(true)
         expect(@vote_two.valid?).to eq(true)
         expect(@vote_three.valid?).to eq(false)
       end
     end
-
-
   end
+
+
+  describe 'after_save' do
+    it "calls 'Post#update_rank' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end
+
 end
